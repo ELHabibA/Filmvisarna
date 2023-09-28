9ea084529176081767433e3438513561b0efdf53
BobbyMoreau <bobbymoreauraquin@gmail.com>
2023-09-28 15:53:33 +0200

    updated styling on background and buttons in sass

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import ReactDOM from 'react-dom/client'
 
-import { Button } from 'react-bootstrap';
+import { Button, Col } from 'react-bootstrap';
 
-function Footer() {
-    return (
-        <div className="fixed-bottom">
-            
-            <div> <Button>Boka</Button></div>
-            <div> <Button>Kontakta oss</Button></div>
-            <div>
-                <p>Made by: Grupp 3 - Elia, Habib, Hampus, Tanya, Artur, Simon & Bobby</p>
-            </div>
-                
-        </div>
+export default function Footer() {
+    return <>
+        <Container className="fixed-bottom footer" expand="xl" fluid>
+            <hr></hr>
+            <Row>
+                <Col><div> <Button>Boka</Button></div></Col>
+                <Col><div><Button>Kontakta oss</Button></div></Col>
+            </Row>
+
+            <Row>
+                <p className='tradeMarkFooter'>Made by: Team 3 - Elia, Habib, Hampus, Tanya, Artur, Simon & Bobby</p>
+            </Row>  
+
+        </Container>
        
-    );
+    </>;
 }
 
-export default Footer;
