6ecede861a425912b1d1ad8ec4c2ff93f3abe530
ArtKoshh <artur.koshmin@hotmail.com>
2023-09-29 18:59:21 +0200

    Clean Up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,18 @@
 import "./sass/main.scss";
 
 // Import some Bootstrap components
-import Background from '../src/components/Background';
-import MovieScroller from '../src/components/MovieScroller';
+import Background from './components/MainSide/Background';
+import MovieScroller from './components/MainSide/Funktions/MovieScroller';
 import MainMenu from './MainMenu';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 
 
 const movieImages = [
-  ('../src/assets/Barbie.jpg'),
-  ('../src/assets/LionKing.jpg'),
-  ('../src/assets/MegaTrench2.jpg'),
-  ('../src/assets/Oppenheimer.jpg'),
-  ('../src/assets/PastLives.jpg'),
+  ('../src/assets/movies/Barbie.jpg'),
+  ('../src/assets/movies/LionKing.jpg'),
+  ('../src/assets/movies/MegaTrench2.jpg'),
+  ('../src/assets/movies/Oppenheimer.jpg'),
+  ('../src/assets/movies/PastLives.jpg'),
   // Add more image URLs as needed
 ];
 
