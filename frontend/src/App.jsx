4815a40876a6424ac162bfa24f84a48000c930ce
quikz <113088508+quikzz@users.noreply.github.com>
2023-10-02 15:09:34 +0200

    Fixed bug app.jsx and package-lock

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import MainMenu from './MainMenu';
-import Footer from "./components/Footer/Footer";
-// Only import your sass in App (not every component)
 import "./sass/main.scss";
 
 // Import some Bootstrap components
 import MainMenu from './MainMenu';
 import Footer from "./components/Footer/Footer";
-import { Container, Row, Col, Button } from 'react-bootstrap';
 import BookingForm from "./components/bookingform/bookingform";
 import { Container, Row, Col } from 'react-bootstrap';
 
