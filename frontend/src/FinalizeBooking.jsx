ae44c1f730a434fc61ccd2f32379821bcae55a01
quikz <113088508+quikzz@users.noreply.github.com>
2023-10-02 15:11:00 +0200

    Finalize booking v1

---
+++ frontend/src/FinalizeBooking.jsx
@@ -0,0 +1,25 @@
+import { Container } from "react-bootstrap";
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import BookingForm from "./components/bookingform/bookingform";
+import BookingSummary from "./components/bookingsummary/bookingsummary";
+
+
+function FinalizeBooking() {
+    return (
+
+        <Container className="mt-5">
+            <Row>
+                <Col md={4} className="mb-3">
+                    <BookingSummary />
+                </Col>
+                <Col md={6} className="mb-3">
+                    <BookingForm />
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default FinalizeBooking;
