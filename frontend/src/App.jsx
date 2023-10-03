a19439931b9e4e31079f29e8bead8c033b9d1d57
ArtKoshh <artur.koshmin@hotmail.com>
2023-10-03 21:14:56 +0200

    MainLayout Ready for Dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,32 +6,32 @@ import "./sass/main.scss";
 
 // Import some Bootstrap components
 import Background from './components/MainSide/Background';
-import MovieScroller from './components/MainSide/Funktions/MovieScroller';
-import MainMenu from './MainMenu';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import MainLayout from './MainLayout';
 
-
-const movieImages = [
-  ('../src/assets/movies/Barbie.jpg'),
-  ('../src/assets/movies/LionKing.jpg'),
-  ('../src/assets/movies/MegaTrench2.jpg'),
-  ('../src/assets/movies/Oppenheimer.jpg'),
-  ('../src/assets/movies/PastLives.jpg'),
-  // Add more image URLs as needed
-];
 
 export default function App() {
-  return <>
-    <MainMenu />
-    <Container className="mt-5">
-      <Row>
-        <MovieScroller movieImages={movieImages} />
-        <Background />
-        <Col>
-          <Outlet />
-        </Col>
-      </Row>
-    </Container>
-    <footer><Footer /></footer>
-  </>;
+  return (
+    <>
+      <MainMenu />
+      <Container className="mt-5">
+        <Row>
+          <Col>
+            <Background />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <MainLayout />
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </>
+  );
 }
+
+
+
+
+
--- frontend/src/components/MainSide/Funktions/MovieScroller.jsx
+++ frontend/src/components/MainSide/Funktions/MovieScroller.jsx
@@ -1,40 +1,30 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import './MovieScroller.css';
 
 function MovieScroller({ movieImages }) {
-    const containerRef = useRef(null);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-    useEffect(() => {
-        const container = containerRef.current;
-        const images = container.querySelectorAll('.movie-image');
-        const totalImages = images.length;
-        let currentIndex = 0;
+    const handlePrevClick = () => {
+        // Handle clicking the "previous" button
+        const prevIndex = (currentIndex - 1 + movieImages.length) % movieImages.length;
+        setCurrentIndex(prevIndex);
+    };
 
-        const rotateImages = () => {
-            currentIndex = (currentIndex + 1) % totalImages;
-
-            images.forEach((image, index) => {
-                const rotationAngle = (index - currentIndex) * (360 / totalImages);
-                image.style.transform = `rotateY(${rotationAngle}deg) translateZ(300px)`;
-            });
-        };
-
-        const rotateInterval = setInterval(rotateImages, 11000); // Adjust the interval as needed
-
-        return () => {
-            clearInterval(rotateInterval);
-        };
-    }, []);
+    const handleNextClick = () => {
+        // Handle clicking the "next" button
+        const nextIndex = (currentIndex + 1) % movieImages.length;
+        setCurrentIndex(nextIndex);
+    };
 
     return (
-        <div className="movie-scroller" ref={containerRef}>
-            <div className="image-list">
-                {movieImages.map((image, index) => (
-                    <div key={index} className="movie-image">
-                        <img src={image} alt={`Movie ${index + 1}`} />
-                    </div>
-                ))}
+        <div className="movie-scroller">
+            <button className="prev-button" onClick={handlePrevClick}>&lt;</button>
+            <div className="image-container">
+                <img className="left-side-image" src={movieImages[currentIndex === 0 ? movieImages.length - 1 : currentIndex - 1]} alt={`Movie ${currentIndex - 1}`} />
+                <img className="center-image" src={movieImages[currentIndex]} alt={`Movie ${currentIndex}`} />
+                <img className="right-side-image" src={movieImages[currentIndex === movieImages.length - 1 ? 0 : currentIndex + 1]} alt={`Movie ${currentIndex + 1}`} />
             </div>
+            <button className="next-button" onClick={handleNextClick}>&gt;</button>
         </div>
     );
 }
