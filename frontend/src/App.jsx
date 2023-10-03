b48fb2f77c0c69a0be299050d0b0e4ebf4997c4d
Elia <89583547+MelonSylt@users.noreply.github.com>
2023-10-03 11:49:02 +0200

    Mergat med den nya Dev
    Allt funkar med få ändringar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import "./sass/main.scss";
 import BasicNavbar from "./components/Navbar/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import BioSeats from './BioSeatsComponents/BioSeats.jsx'
 import { Container, Row, Col } from 'react-bootstrap';
 
 export default function App() {
@@ -16,7 +17,8 @@ export default function App() {
     <Container className="mt-5 body">
       <Row>
         <Col>
-          <h1>Hello!</h1>
+                  <h1>Hello!</h1>
+                  <BioSeats />
           <p>There you are...</p>
         </Col>
       </Row>
