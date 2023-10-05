c4efd11525b6919b49269364cc4b93452c353be1
pekz <peky@live.se>
2023-10-05 10:23:35 +0200

    navbar changes
    navbar changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
 import { Outlet } from 'react-router-dom';
-import MainMenu from './MainMenu';
 import Footer from "./components/Footer/Footer";
 import "./sass/main.scss";
 import BasicNavbar from "./components/Navbar/Navbar";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { Container, Row, Col } from 'react-bootstrap';
 
 export default function App() {
