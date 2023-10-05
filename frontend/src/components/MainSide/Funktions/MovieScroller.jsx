2ab766a959d56ceb548ca54c893986caf21d2efa
ArtKoshh <artur.koshmin@hotmail.com>
2023-10-05 15:12:31 +0200

    Image onClick - Redirects Correctly

--- frontend/src/components/MainSide/Funktions/MovieScroller.jsx
+++ frontend/src/components/MainSide/Funktions/MovieScroller.jsx
@@ -1,30 +1,44 @@
 import React, { useState } from 'react';
 import './MovieScroller.css';
 
-function MovieScroller({ movieImages }) {
+function MovieScroller({ movieImages, movieIds }) {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const handlePrevClick = () => {
-        // Handle clicking the "previous" button
         const prevIndex = (currentIndex - 1 + movieImages.length) % movieImages.length;
         setCurrentIndex(prevIndex);
     };
 
     const handleNextClick = () => {
-        // Handle clicking the "next" button
         const nextIndex = (currentIndex + 1) % movieImages.length;
         setCurrentIndex(nextIndex);
     };
 
     return (
-        <div className="movie-scroller">
-            <button className="prev-button" onClick={handlePrevClick}>&lt;</button>
-            <div className="image-container">
-                <img className="left-side-image" src={movieImages[currentIndex === 0 ? movieImages.length - 1 : currentIndex - 1]} alt={`Movie ${currentIndex - 1}`} />
-                <img className="center-image" src={movieImages[currentIndex]} alt={`Movie ${currentIndex}`} />
-                <img className="right-side-image" src={movieImages[currentIndex === movieImages.length - 1 ? 0 : currentIndex + 1]} alt={`Movie ${currentIndex + 1}`} />
+        <div className="movie-scroller-container">
+            <div className="movie-scroller">
+                <button className="prev-button" onClick={handlePrevClick}>&lt;</button>
+                <div className="image-container">
+                    <img
+                        className="left-side-image"
+                        src={movieImages[currentIndex === 0 ? movieImages.length - 1 : currentIndex - 1]}
+                        alt={`Movie ${currentIndex - 1}`}
+                    />
+                    <a href={`/detaljsidan/${movieIds[currentIndex]}`} className="center-image-link">
+                        <img
+                            className="center-image"
+                            src={movieImages[currentIndex]}
+                            alt={`Movie ${currentIndex}`}
+                        />
+                    </a>
+                    <img
+                        className="right-side-image"
+                        src={movieImages[currentIndex === movieImages.length - 1 ? 0 : currentIndex + 1]}
+                        alt={`Movie ${currentIndex + 1}`}
+                    />
+                </div>
+                <button className="next-button" onClick={handleNextClick}>&gt;</button>
             </div>
-            <button className="next-button" onClick={handleNextClick}>&gt;</button>
         </div>
     );
 }
