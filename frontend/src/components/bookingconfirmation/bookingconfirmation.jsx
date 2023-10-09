aaefc7d39bb1dd9fdf22066755ccb9a4c7cf9156
quikz <113088508+quikzz@users.noreply.github.com>
2023-10-09 21:16:20 +0200

    Working on responsive behavior and content för booking confirmation

--- frontend/src/components/bookingconfirmation/bookingconfirmation.jsx
+++ frontend/src/components/bookingconfirmation/bookingconfirmation.jsx
@@ -3,29 +3,35 @@ import BookingNumber from '../BookingNumber';
 
 function BookingConfirmation() {
     return (
-        <Container>
+        <Container className="">
 
             <Row>
-                <Col sm={6} className="bg-secondary p-3 rounded">
+                <Col sm={6} md={12} lg={12} className="bg-secondary p-4 rounded"> 
+                <h1>Tack för din bokning!</h1>
+                
+                <p className="fw-italic">Bokningsbekräftelsen har skickats till din mejl.</p>
                     <Image src="holder.js/200x200" rounded />
-                    <div className="fw-bold mb-3">Titel</div>
-                    <div className="mb-4">Oppenheimer</div>
-                    <div className="fw-bold mb-3">Datum</div>
-                    <div className="mb-4">2023-10-05 19:30</div>
-                    <div className="fw-bold mb-3">Biljetter</div>
-                    <div className="mb-4">
-                        
-                        <div>Bokningsnummer: <BookingNumber /></div>
+                    <div className="fw-bold mb-2">Titel</div>
+                    <div className="mb-3">Oppenheimer</div>
+                    <div className="fw-bold mb-2">Datum</div>
+                    <div className="mb-3">2023-10-05 19:30</div>
+                    <div className="fw-bold mb-2">Biljetter</div>
+                    <div className="mb-3">
                         <div>2 st vuxna</div>
                         <div>1 st barn</div>
                         <div>1 st pensionär</div>
                     </div>
-                    <div className="fw-bold mb-3">Platser</div>
-                    <div className="mb-4">
+                    <div className="fw-bold mb-2">Bokningsnummer</div>
+                    <div className="mb-3">
+                        <BookingNumber/>
+                    </div>
+                    
+                    <div className="fw-bold mb-2">Platser</div>
+                    <div className="mb-3">
                         <div>A1, A2, A3</div>
                     </div>
-                    <div className="fw-bold mb-3">Email</div>
-                    <div className="mb-4">
+                    <div className="fw-bold mb-2">Email</div>
+                    <div className="mb-3">
                         <div>customer@gmail.com</div>
                     </div>
                     <hr />
@@ -34,8 +40,10 @@ function BookingConfirmation() {
                 </Col>
             </Row>
         </Container>
+        
 
     )
 }
+
 
 export default BookingConfirmation;
