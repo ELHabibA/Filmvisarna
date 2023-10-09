f7daeeaf14eedaf1b10e0099fe061fa0800a206a
BobbyMoreau <bobbymoreauraquin@gmail.com>
2023-10-09 08:48:59 +0200

    connected with database and first query worked

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+//const mysql = require('mysql');
+const db = require('./db.js');
+const connection = require('./db.js');
+
+
 const app = express();
 const port = 8002; 
 
@@ -11,3 +16,32 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Servern lyssnar på port ${port}`);
 });
+
+function dbQuery(query, params = []) {
+
+  if(params.constructor == Object) {
+    let newParams = [];
+    query = query.replace(/:\w*/g, param => {
+      newParams.push(params[param.slice(1)]);
+    return '?';
+  });
+  params = newParams; 
+  }
+
+return new Promise(resolve => {
+  connection.execute(query, params, (err, result) =>
+    resolve(err || result)
+  );
+  });
+}
+
+async function runExampleQueries() {
+
+  // query with no parameters
+  let result1 = await dbQuery(
+    'SELECT * FROM ticketType'
+  );
+  console.log('\n', result1);
+}
+
+ runExampleQueries();
