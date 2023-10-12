d011a8e75ab4d768e50c982ac66987296ae8363b
quikz <113088508+quikzz@users.noreply.github.com>
2023-10-12 16:39:31 +0200

    Draft #3 email template almost done

--- backend/nodemailer/sendEmail.js
+++ backend/nodemailer/sendEmail.js
@@ -1,25 +1,106 @@
 import nodeMailer from "nodemailer";
 import { email as fromEmail, password } from './loginCredentials.js';
 
-export default async function sendEmail({ header, email, title, ticketType, seats, price }) {
+export default async function sendEmail({email, title, date, ticketType, seats, price, bookingNo }) {
 
-    const html = `
+    const html = `<!DOCTYPE html>
+    <html>
+    
+    <head>
+
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                font-size: 18px;
+                padding: 20px;
+            }
+        </style>
+    </head>
     
     <body style="background-color: #181619;">
-    <font color="#DDA74F">
-    <header>
-        <h1>${header} </h1>
-        <h3> Här kommer din bokning </h3><hr/>
-    </header>
-    <body>
-        <p>Titel: ${title}</p>
-        <p>Biljetter: ${ticketType}</p>
-        <p>Stolar: ${seats}</p>
-        <p>Pris: ${price}</p><hr/>
+        <header style="text-align: center; background-color: #DDA74F; padding: 5px;">
+            <h1 style="color: #181619;">Tack för din bokning!</h1>
+            <h3 style="color: #181619;">Här kommer din bokningsbekräftelse</h3>
+            <hr style="margin-top: 20px; border: 1px solid #DDA74F;" />
+        </header>
+        <hr style="border: 1px solid #DDA74F; margin-bottom: 10px;" />
+    
+        <table style="text-align: left; color: #DDA74F; margin-top: 50px; padding: 20px;">
+            <tr>
+                <td style="vertical-align: top; width: 20%;">
+                    <img 
+                        style="display: block; margin-right: 10px;"
+                        src="https://pbs.twimg.com/media/FvUVt3hXgAAxP1H?format=jpg&name=900x900" 
+                        alt="A Group of Ring-tailed Lemurs" 
+                        width="150px"
+                        height="200px"
+                    />
+                </td>
+                <td style="vertical-align: top; width: 80%;">
+                    <table>
+                        <tr>
+                            <th>&nbsp;</th>
+                            <th style="margin: 10px;">
+                                <b>Titel:</b>
+                            </th>
+                            <td>
+                                ${title}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>&nbsp;</th>
+                            <th>
+                                <b>Datum:</b>
+                            </th>
+                            <td>
+                                ${date}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>&nbsp;</th>
+                            <th>
+                                <b>Biljetter:</b>
+                            </th>
+                            <td>
+                                ${ticketType}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>&nbsp;</th>
+                            <th>
+                                <b>Stolar:</b>
+                            </th>
+                            <td>
+                                ${seats}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>&nbsp;</th>
+                            <th>
+                                <b>Pris:</b>
+                            </th>
+                            <td>
+                                ${price}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>&nbsp;</th>
+                            <th>
+                                <b>Bokningsnummer:</b>
+                            </th>
+                            <td>
+                                ${bookingNo}
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <hr style="border: 1px solid #DDA74F; margin-top: 50px;" />
+        <footer style="text-align: center; background-color: #DDA74F; color: #181619; padding: 10px;"><b>Filmvisarna AB</b></footer>
     </body>
-    <footer>
-        Filmvisarna AB
-    </footer>
+    </html>
+                   
 `;
     const text = `Filmvisarna - Här kommer din bokning`;
 
--- backend/nodemailer/testSendEmail.js
+++ backend/nodemailer/testSendEmail.js
@@ -1,10 +1,11 @@
 import sendEmail from './sendEmail.js';
 
 sendEmail({
-    header: 'Tack för din störtsköna bokning!',
     email: 'hakansson.hampus@gmail.com',
-    title: 'Exempel the movie',
+    title: 'Oppenheimer',
     ticketType: '1 barn, 2 vuxen, 1 pensionär',
     seats: ['1A', '2A', '3A', '4A'],
-    price: 450 + ' kr'
+    price: 450 + ' kr',
+    date: '2023-12-31',
+    bookingNo: '2FDX7A9E'
 });
