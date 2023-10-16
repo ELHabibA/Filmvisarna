09c2eadc3ed9e0a0d642c01d9ef68b26d3c85741
BobbyMoreau <bobbymoreauraquin@gmail.com>
2023-10-16 16:14:14 +0200

    romoved olid movie-listing

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -103,7 +103,6 @@ const Movies = () => {
                 }}
             />
             <h1>Filmer</h1>
-            <MoviesList />
             <div>
                 {/* Filtrering för ålder */}
                 <Col xs={12} sm={6} md={4} lg={3} style={{ marginBottom: '15px' }}>
@@ -142,38 +141,10 @@ const Movies = () => {
                         </Dropdown.Menu>
                     </Dropdown>
                 </Col>
+                            
                 {/*Film - lista*/}
-                <div className="movie-list">
-                    {filteredMovies.map((movie) => (
-                        <div key={movie.id} className="movie-item" style={{ marginBottom: '10px' }}>
-                            <Link to={`/detaljsidan/${movie.id}`} style={linkStyle}>
-                                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '10px' }}>
-                                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                                        <div style={{ marginRight: '20px' }}>
-                                            <img
-                                                src={movie.poster}
-                                                alt="Movie Poster"
-                                                className="img-fluid"
-                                                style={{
-                                                    width: '150px',
-                                                    height: 'auto',
-                                                    border: '2px solid black',
-                                                }}
-                                            />
-                                        </div>
-                                        <div>
-                                            <h2 style={{ fontSize: '20px', marginBottom: '5px' }}>{movie.title}</h2>
-                                            <p style={{ margin: 0, fontSize: '16px' }}>{movie.genre} I {movie.duration} I {movie.ageRating}</p>
-                                        </div>
-                                    </div>
-                                    <Link to={`/detaljsidan/${movie.id}`}>
-                                        <button className="btn btn-primary" style={{ marginLeft: '10px' }}>Detaljer</button>
-                                    </Link>
-                                </div>
-                            </Link>
-
-                        </div>
-                    ))}
+                <div>
+                    <MoviesList />
                 </div>
             </div>
         </div>
