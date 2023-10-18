2efe16462cc423f83185c9f4e9e3332d94f4ff6c
Elhabib Asmama <elhabibasmama@gmail.com>
2023-10-18 15:28:00 +0200

    Log in function fixed

--- src/LogIn.jsx
+++ src/LogIn.jsx
@@ -1,29 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Card, Button, Form } from 'react-bootstrap';
+import {useNavigate} from 'react-router-dom';
 
-const LogIn = () => {
-  const cardStyle = { backgroundColor: 'rgba(211, 211, 211, 0.6)', maxWidth: '600px', margin: '0 auto' };
-  const loginHeaderText = 'Logga In';
+async function postData(url = "", data = {}) {
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+const Login = () => {
+
+
+  const navigate = useNavigate();
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    const data = { email, password };
+    const response = await postData('/api/login', data);
+
+    console.log("DA LOGIN RESPONSE", response)
+    if (response.error) {
+      // Handle login error 
+      console.error(response.error);
+    } else {
+      // Go to movies page on successful login
+      navigate('/Filmer');
+    }
+  };
 
   return (
     <Container className="mt-5">
-      <Card style={cardStyle}>
+      <Card>
         <Card.Body className="text-center">
-          <h2>{loginHeaderText}</h2>
+          <h2>Logga In</h2>
           <Form className="rectangle-form">
             <Form.Group>
               <Form.Label>E-post</Form.Label>
-              <Form.Control type="text" placeholder="E-post" />
+              <Form.Control
+                type="text"
+                placeholder="E-post"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </Form.Group>
             <Form.Group>
               <Form.Label>Lösenord</Form.Label>
-              <Form.Control type="password" placeholder="Lösenord" />
+              <Form.Control
+                type="password"
+                placeholder="Lösenord"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </Form.Group>
           </Form>
         </Card.Body>
         <Card.Footer className="text-center">
-          <Button variant="primary" type="submit">
+          <Button variant="primary" onClick={handleLogin}>
             Logga In
           </Button>
           <p className="mt-3">
@@ -33,6 +74,6 @@ const LogIn = () => {
       </Card>
     </Container>
   );
-}
+};
 
-export default LogIn;
+export default Login;
