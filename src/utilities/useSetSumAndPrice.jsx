0f50fc843d7a20975c9101ac647109aab69d4aee
BobbyMoreau <bobbymoreauraquin@gmail.com>
2023-10-19 08:28:43 +0200

    refactored code

---
+++ src/utilities/useSetSumAndPrice.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+const useSetSumAndPrice = (adults, kids, retired, onSumChange) => {
+      useEffect(() => {
+        const newSum = adults + kids + retired;
+        if (typeof onSumChange === 'function') {
+            onSumChange(newSum);
+        }
+    }, [adults, kids, retired, onSumChange]);
+
+};
+
+export default useSetSumAndPrice;
